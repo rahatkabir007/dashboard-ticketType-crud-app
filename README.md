3a673e88230125798c853e958dc115ed05187870
rahatkabir007 <rahatkabir04@gmail.com>
2023-05-30 13:35:24 +0600

    final readme done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 TICKET CRUD APP
+Features:
+
+Add Ticket Type and Description with a stylish popup modal. Receive a success toast message after adding.
+Edit Ticket Type and Description with an elegant popup modal. Receive a success toast message after editing.
+Remove Ticket Type with a sophisticated delete option in the popup modal. Receive a success toast message after removing.
+
+Bonus Feature:
+
+Paginate and display data in a sleek and organized manner. Show 5 data entries per page.
+
+Please note that all CRUD operations are implemented using Redux for efficient state management.
--- src/pages/Homepage/Layout/RightContent/RightTable/RightTable.jsx
+++ src/pages/Homepage/Layout/RightContent/RightTable/RightTable.jsx
@@ -8,7 +8,7 @@ import { useSelector } from 'react-redux';
 const RightTable = ({ handleModalOpen, handleClose }) => {
     const { ticketTypes } = useSelector(state => ({ ...state.ticketType }))
     const [currentPage, setCurrentPage] = useState(1);
-    const [dataPerPage] = useState(3);
+    const [dataPerPage] = useState(5);
     const tableHeaders = [
         "Ticket No.",
         "Ticket Type",
@@ -47,57 +47,61 @@ const RightTable = ({ handleModalOpen, handleClose }) => {
                                     </thead>
                                     {/* -----------Plz Attention ,Table body/Row start here -------------- */}
                                     <tbody>
-                                        {currentTableTypes?.map((data, index) => (
-                                            <tr key={index} className="even:bg-gray-50 odd:bg-white text-center">
-                                                <td className="px-3 py-5 text-sm">
-                                                    <p className="text-gray-900 ">{index + 1}</p>
-                                                </td>
-                                                <td className="px-3 py-5 text-sm">
-                                                    <p className="text-gray-900 ">{data?.ticketType}</p>
-                                                </td>
-                                                <td className="px-3 py-3  text-sm capitalize">
-                                                    <p className="text-gray-900 ">
-                                                        {data?.description}
-                                                    </p>
-                                                </td>
-                                                <td className="px-2 py-3  text-sm">
-                                                    <button
-                                                        onClick={() => handleModalOpen(<TicketForm submitbtn="Edit Ticket" handleClose={handleClose} data={data} />)}
-                                                    >
-                                                        <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
-                                                            <span
-                                                                style={{ boxShadow: "0 2px 6px #acb5f6" }}
-                                                                className="h-8 w-8  inset-0 bg-blue-700   rounded  relative text-white flex justify-center items-center"
-                                                            >
-
-                                                                <FaEdit />
-                                                            </span>
-                                                        </span>
-                                                    </button>
-                                                    <button
-                                                        onClick={() => handleModalOpen(<DeleteTicketForm handleClose={handleClose} id={data?.id} />)}
-                                                    >
-                                                        <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
-                                                            {data && (
+                                        {currentTableTypes?.map((data, index) => {
+                                            const uniqueIndex = (currentPage - 1) * dataPerPage + index + 1;
+                                            console.log("🚀 ~ file: RightTable.jsx:52 ~ {currentTableTypes?.map ~ uniqueIndex:", uniqueIndex, index, indexOfLastData, indexOfFirstData + 1, Math.min(indexOfLastData, ticketTypes.length))
+                                            return (
+                                                <tr key={index} className="even:bg-gray-50 odd:bg-white text-center">
+                                                    <td className="px-3 py-5 text-sm">
+                                                        <p className="text-gray-900 ">{uniqueIndex > indexOfLastData ? uniqueIndex - (indexOfLastData - dataPerPage) : uniqueIndex}</p>
+                                                    </td>
+                                                    <td className="px-3 py-5 text-sm">
+                                                        <p className="text-gray-900 ">{data?.ticketType}</p>
+                                                    </td>
+                                                    <td className="px-3 py-3  text-sm capitalize">
+                                                        <p className="text-gray-900 ">
+                                                            {data?.description}
+                                                        </p>
+                                                    </td>
+                                                    <td className="px-2 py-3  text-sm">
+                                                        <button
+                                                            onClick={() => handleModalOpen(<TicketForm submitbtn="Edit Ticket" handleClose={handleClose} data={data} />)}
+                                                        >
+                                                            <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
                                                                 <span
-                                                                    style={{ boxShadow: "0 2px 6px #fd9b96" }}
-                                                                    className="h-8 w-8  inset-0 bg-red-500   rounded  relative text-white flex justify-center items-center"
+                                                                    style={{ boxShadow: "0 2px 6px #acb5f6" }}
+                                                                    className="h-8 w-8  inset-0 bg-blue-700   rounded  relative text-white flex justify-center items-center"
                                                                 >
-                                                                    <FaTrash />
+
+                                                                    <FaEdit />
                                                                 </span>
-                                                            )}
-                                                        </span>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        ))}
+                                                            </span>
+                                                        </button>
+                                                        <button
+                                                            onClick={() => handleModalOpen(<DeleteTicketForm handleClose={handleClose} id={data?.id} />)}
+                                                        >
+                                                            <span className="relative inline-block px-1 py-1 font-semibold text-green-900 leading-tight">
+                                                                {data && (
+                                                                    <span
+                                                                        style={{ boxShadow: "0 2px 6px #fd9b96" }}
+                                                                        className="h-8 w-8  inset-0 bg-red-500   rounded  relative text-white flex justify-center items-center"
+                                                                    >
+                                                                        <FaTrash />
+                                                                    </span>
+                                                                )}
+                                                            </span>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
                                     </tbody>
                                 </table>
                                 {/* ---------- table footer  ------------------------------- */}
                                 <div className="px-5 py-5 border-t flex justify-between items-center">
                                     <div>
                                         <span className="text-xs xs:text-sm text-gray-900">
-                                            Showing 1 to {dataPerPage} of {ticketTypes?.length} Entries
+                                            Showing {indexOfFirstData + 1} to {Math.min(indexOfLastData, ticketTypes.length)} of {ticketTypes?.length} Entries
                                         </span>
                                     </div>
                                     <div className="inline-flex  xs:mt-0">
